9311260a708c8d69afd6ffc2112e9fa23b14ad15
Christian Giupponi <christian.giupponi@stilogo.it>
2013-09-19 17:48:19 +0200

    First Commit
    First commit

---
+++ language/italian/codice_fiscale_lang.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+// labels
+$lang['streams:codice_fiscale.name']    = "Codice fiscale";
+$lang['streams:codice_fiscale.instructions']    = "Inserisci il Codice Fiscale";
